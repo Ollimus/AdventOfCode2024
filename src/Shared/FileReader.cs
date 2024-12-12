ace8c4cc12b8b2e235aba3012a18aa4a944cf5a9
Olli <Olli.Hirvonen@hotmail.fi>
2024-12-12 02:16:36 +0200

    Setting up the basic reader and folder structure and stuff before starting up the task

---
+++ src/Shared/FileReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared;
+
+public interface IFileReader
+{
+    IEnumerable<string> Read();
+}
+
+public class FileReader : IFileReader
+{
+    public readonly string FilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+
+    public IEnumerable<string> Read(string path = "")
+        => File.ReadAllText
+
+
+    string[] input = File.ReadAllLines(FilePath.Combine(AppDomain.CurrentDomain.BaseDirectory, "PuzzleInput.dat"));
+
+}
