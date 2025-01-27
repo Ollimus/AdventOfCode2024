ab3f9868e964d49293595135c599039a20c7b733
Olli <Olli.Hirvonen@hotmail.fi>
2025-01-28 01:02:14 +0200

    Init

---
+++ src/Day2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Shared;
+
+/*
+ * Task description within README.md
+*/
+
+var input = new FileReader().Read();
+//var test = input.SelectMany(input, (x, test) => Console.WriteLine($"{x} {test}"));
+
+
+Console.ReadKey();
+
+public class Day2
+{
+
+    public void Part1()
+    {
+    }
+
+    public void Part2()
+    {
+    }
+}
