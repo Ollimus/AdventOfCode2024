cd8f3db048d2d2ef44eb7a3c237b71c1a664e10b
Olli <Olli.Hirvonen@hotmail.fi>
2025-02-03 20:09:02 +0200

    Inefficient solution to part 1 (O(2n)) but I did want to train doing things with linq.

--- src/Day2/Program.cs
+++ src/Day2/Program.cs
@@ -19,15 +19,20 @@ public class Day2
             .ToList())
         .ToList();
 
+        // Find every succesful report and finally add them together as sum.
         var result = input.Select(rows =>
         {
+            // Checks whether values are increasing or decreasing.
+            // Unable to do it in one check in a smart way since collections can be unordered.
             var isAscending = rows.Zip(rows.Skip(1), (x, y) => x <= y).All(x => x);
             var isDescending = rows.Zip(rows.Skip(1), (x, y) => x >= y).All(x => x);
 
+            // Since ALL values are not ascending nor descending, we know that values within collection jump back and forth.
+            // Ie: 3 5 3 6 as a collection does not descend or ascend.
             if (!isAscending && !isDescending)
                 return 0;
 
-
+            // We check whether the first value on the collection is within parameters (more than 1, less than 3).
             var isWithinParameters = rows.Zip(rows.Skip(1), (x, y) =>
             {
                 var difference = Math.Abs(x - y);
@@ -35,53 +40,12 @@ public class Day2
             }).All(x => x);
 
             return isWithinParameters ? 1 : 0;
-        })
-            .Sum();
+        }).Sum();
 
         Console.WriteLine("Result is: " + result);
     }
 
     public void Part2()
     {
-    }
-}
-
-public class ValidationClass
-{
-    public int ThresholdParameter { get; set; } = 0;
-    public bool IsAscending { get; set; } = true;
-    public bool IsValid { get; set; } = true;
-
-    public ValidationClass(int thresholdParameter, bool isAscending)
-    {
-        ThresholdParameter = thresholdParameter;
-        IsAscending = isAscending;
-    }
-
-    public bool AreValuesAscending(int x, int y)
-    {
-        if (IsAscending)
-        {
-            if (x > y)
-            {
-                IsValid = false;
-                IsAscending = false;
-                return false;
-            }
-
-            return true;
-        }
-
-        else
-        {
-            if (x < y)
-            {
-                IsValid = false;
-                IsAscending = true;
-                return true;
-            }
-
-            return false;
-        }
     }
 }
