993558e793093d720ed1d904960e44c4882caa6e
Olli <Olli.Hirvonen@hotmail.fi>
2025-02-17 02:15:48 +0200

    Working solution for part 2. Will check it out and refactor it later.

--- src/Day3/Program.cs
+++ src/Day3/Program.cs
@@ -3,9 +3,16 @@
 */
 
 using Shared;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.Text.RegularExpressions;
+using System.Transactions;
 
-new Day3().Part1();
+
+
+
+//new Day3().Part1();
 new Day3().Part2();
 
 Console.ReadKey();
@@ -14,12 +21,15 @@ Console.ReadKey();
 
 public class Day3
 {
+    public const string @do = "do()";
+    public const string dont = "don't()";
+
     public void Part1()
     {
         // Find each instances of:
         // mul({value1}, {value2})
-        var regex = new Regex(@"mul\((\d+,\d+)\)");
-        var extractedStrings = ExtractValues(new FileReader().ReadAllText(), regex);
+        var regexPattern = new Regex(@"mul\((\d+,\d+)\)");
+        var extractedStrings = ExtractValues(new FileReader().ReadAllText(), regexPattern);
 
         var result = extractedStrings
              .Select(x => x.Split(',', StringSplitOptions.RemoveEmptyEntries)
@@ -40,29 +50,116 @@ public class Day3
 
     public void Part2()
     {
+
         // Find each instances of:
         // mul({value1}, {value2})
         // do()
         // don't
-        var regex = new Regex(@"mul\((\d+,\d+)\)|do\(\)|don't\(\)");
-        var extractedStrings = ExtractValues(new FileReader().ReadAllText(), regex);
+        Regex regexPattern = new Regex(@"mul\((\d+,\d+)\)|do\(\)|don't\(\)");
 
+        var result = ExtractValues(new FileReader().ReadAllText(), regexPattern)
+            .Aggregate((Sum: 0, ProcessCurrentValue: true), (acc, nextValue) =>
+            {
+                // Is true, false or null. The latter if value is an integer.
+                var processNextValue = IsDoOrDont(nextValue);
 
+                // If processNextValue is null, it means it is an integer and therefore can be processed if the current instruction is to process.
+                if (acc.ProcessCurrentValue && processNextValue is null)
+                {
+                    // Split string into an array, parse it into ints and multiply.
+                    var multipliedSum = nextValue.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .Aggregate((x, y) => x * y);
 
-        //Console.WriteLine("Result is: " + result);
+                    // Return acc sum with the multiplied sum and assign previous instruction (true in this case).
+                    return (acc.Sum + multipliedSum, acc.ProcessCurrentValue);
+                }
+
+                // If value was an integer but instruction was to ignore it, we assign current instruction (false in this case)
+                processNextValue ??= acc.ProcessCurrentValue;
+                
+                // Return previous values unchanged.
+                return (acc.Sum, processNextValue.Value);
+            });
+
+        Console.WriteLine("Result is: " + result.Sum);
     }
 
-    static List<string> ExtractValues(string input, Regex regex)
+    static List<string> ExtractValues(string input, Regex regexPattern)
     {
         List<string> values = []; // Just trying to remember how to use the new way of creating lists.
-        MatchCollection matches = regex.Matches(input);
+        MatchCollection matches = regexPattern.Matches(input);
 
         foreach (Match match in matches)
         {
-            values.Add(match.Groups[1].Value);
+            // value 0 is for 'don't() and 'do()'. Value 1 in these are empty.
+            // Value 1 is for mul values. Value 0 contains the whole mul -value but value1 contains string of ie. "11,12" 
+            var value = match.Groups.Values.Any(x => string.IsNullOrEmpty(x.Value))
+                ? match.Groups[0].Value : match.Groups[1].Value;
+
+            values.Add(value);
         }
 
         return values;
     }
+
+    // True
+    // False
+    // Null
+    private static bool? IsDoOrDont(string input)
+        => !(input == @do || input == dont) ?null : input == @do;
 }
 
+
+
+//.Select(x =>
+//{
+
+
+//    return new
+//    {
+//        Original = x, // Always keep the original value as a string,
+//        Command = x == @do ? @do : x == dont ? dont : null,
+//        Computed = (x == @do || x == dont) ? (int?)null :
+//                   x.Split(',', StringSplitOptions.RemoveEmptyEntries)
+//                    .Select(int.Parse)
+//                    .Aggregate((a, b) => a * b) // Keep it as an int
+//    };
+//});
+
+// Input made into format of:
+// 
+//var extractedValues = ExtractValues(new FileReader().ReadAllText(), regexPattern)
+//    .Select(x =>
+//    {
+
+
+//        return new
+//        {
+//            Original = x, // Always keep the original value as a string,
+//            Command = x == @do ? @do : x == dont ? dont : null,
+//            Computed = (x == @do || x == dont) ? (int?)null :
+//                       x.Split(',', StringSplitOptions.RemoveEmptyEntries)
+//                        .Select(int.Parse)
+//                        .Aggregate((a, b) => a * b) // Keep it as an int
+//        };
+//    });
+
+//var result = extractedValues.Aggregate((sum: 0, calculateNextValue: false), (acc, nextvalue) =>
+//{
+//    if (!acc.calculateNextValue)
+//        return (acc.sum, (nextvalue.Command != null ? nextvalue.Command : acc.calculateNextValue));
+
+//    return ((acc.sum + nextvalue.Computed), (nextvalue.Command != null ? nextvalue.Command : acc.calculateNextValue));
+//});
+
+
+//var result = extractedValues.Aggregate((sum: 0, calculateNextValue: false), (acc, nextvalue) =>
+//{
+//    if (!acc.calculateNextValue)
+//        return (acc.sum, nextvalue. == @do ? true : nextvalue == dont ? false : acc.calculateNextValue);
+
+//    var canConvert = int.TryParse(nextvalue, out var intValue);
+
+//    return canConvert ? (acc.sum + intValue, nextvalue == @do ? true : acc.calculateNextValue) : (acc.sum, acc.calculateNextValue);
+//});
